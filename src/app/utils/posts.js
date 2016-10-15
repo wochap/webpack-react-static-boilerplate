6258af4bc4627cf957731086fe7ac0e375de9ddc
wochap <gean.marroquin@gmail.com>
2016-10-15 16:20:33 -0500

    [feat] add front-matter

--- src/app/utils/posts.js
+++ src/app/utils/posts.js
@@ -1,12 +1,15 @@
-const postsContext = require.context('src/posts', true, /\.md$/)
-const postsRoutes = postsContext.keys().map(function (path) {
+const postsContext = require.context('!!front-matter-loader!src/posts', true, /\.md$/)
+
+const posts = postsContext.keys().map(function (path) {
   // extract file name from path file
   let fileName = (path.split('/').pop().split('.'))[0]
+  let frontMatter = postsContext(`./${fileName}.md`)
 
   return {
     slug: fileName,
-    title: fileName.replace(/-/g, ' ')
+    title: fileName.replace(/-/g, ' '),
+    ...frontMatter
   }
 })
 
-export default postsRoutes
+export default posts
