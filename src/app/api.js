d97d3bb96111ed88f7e53685cfea2f99c9e01ef0
wochap <gean.marroquin@gmail.com>
2016-11-11 09:09:34 -0500

    [fix] responses api

--- src/app/api.js
+++ src/app/api.js
@@ -2,24 +2,7 @@
  * get all posts' front-matter
  * @return {array} all posts
  */
-export function getPosts (fileName) {
-  return new Promise((resolve, reject) => {
-    try {
-      let module = require(`src/posts/${fileName}.md`)
-
-      module(resolve)
-    } catch (error) {
-      console.warn(`Error: Cannot find '${fileName}.md' post file.`) // eslint-disable-line
-      reject(error)
-    }
-  })
-}
-
-/**
- * get post's bodyHTML and front-matter
- * @return {object} post
- */
-export function getPost () {
+export function getPosts () {
   return new Promise((resolve, reject) => {
     try {
       const postsContext = require.context('!!front-matter-loader!src/posts', true, /\.md$/)
@@ -29,14 +12,43 @@ export function getPost () {
         let fileName = (path.split('/').pop().split('.'))[0]
         let frontMatter = postsContext(`./${fileName}.md`)
 
-        return Object.assign({}, {
-          slug: fileName,
-          title: fileName.replace(/-/g, ' ')
-        }, frontMatter)
+        return {
+          frontMatter: {
+            slug: fileName,
+            title: fileName.replace(/-/g, ' '),
+            ...frontMatter
+          }
+        }
       })
 
       resolve(posts)
     } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+/**
+ * get post's bodyHTML and front-matter
+ * @return {object} post
+ */
+export function getPost (fileName) {
+  return new Promise((resolve, reject) => {
+    try {
+      let module = require(`src/posts/${fileName}.md`)
+
+      module((result) => {
+        resolve({
+          ...result,
+          frontMatter: {
+            slug: fileName,
+            title: fileName.replace(/-/g, ' '),
+            ...result.frontMatter
+          }
+        })
+      })
+    } catch (error) {
+      console.warn(`Error: Cannot find '${fileName}.md' post file.`) // eslint-disable-line
       reject(error)
     }
   })
